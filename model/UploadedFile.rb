41748830b8f41e3c1460d516a7d64882c1166adb
kaakaa_hoe <stooner.hot@gamil.com>
2013-12-14 10:17:57 +0900

    refactoring model class

--- model/UploadedFile.rb
+++ model/UploadedFile.rb
@@ -4,7 +4,6 @@ require 'haml'
 class UploadedFile
 
 	def initialize(filename)
-		p filename
 		@filename = filename
 	end
 
@@ -16,45 +15,45 @@ class UploadedFile
 
 	def savePdf
 		home = File.join(File.dirname(__FILE__), '..')
-		targetDirPath = File.join(home, "uploads", "#{@filename.split('.')[0]}")
-		Dir.mkdir(targetDirPath) if !Dir.exists?(targetDirPath)
-		pdfPath = "#{targetDirPath}/#{@filename.split('_')[1]}.pdf"
+		pdfPath = "#{targetDirPath}/#{@filename.split('_')[1..-1].join('_')}.pdf"
 		`java -jar #{home}/javalib/jodconverter-cli-2.2.2.jar #{uploadedFilePath} #{pdfPath}`
 	end
 
 	def savePng
 		home = File.join(File.dirname(__FILE__), '..')
-		targetDirPath = File.join(home, "uploads", "#{@filename.split('.')[0]}")
 		Dir.mkdir(targetDirPath) if !Dir.exists?(targetDirPath)
 		Dir.mkdir(File.join(targetDirPath, "png"))
 		
 		# make pdf file path
 		# TODO extract method
-		pdf = File.join(home, "uploads", "#{@filename.split('.')[0]}", "#{@filename.split('_')[1]}.pdf")
+		pdf = File.join(targetDirPath, "#{@filename.split('_')[1]}.pdf")
 		pdf_magick = Magick::ImageList.new(pdf){ self.density = 150 }
 		pdf_magick.each_with_index { |val, index|
-			p "convering pdf to png (#{index+1}/#{pdf_magick.size})"
-			pdf_magick[index].write(File.join(targetDirPath, "png", "page#{format("%03d", index+1)}.png")){ self.quality = 100 }
+			pdf_magick[index].write(File.join(targetDirPath, "png", "page#{format("%03d", index+1)}.png"))
 		}
 	end
 
 	def makeHtml
 		home = File.join(File.dirname(__FILE__), '..')
 		images = Array.new
-		 Dir.glob(File.join(home, "uploads", "#{@filename.split('.')[0]}", "png", "*.png")).sort!.each{ |d|
+		Dir.glob(uploadedDirPath, "png", "*.png")).sort!.each{ |d|
 			images << d.gsub!(/#{home}/, '')
 		}
 		engine = nil
 		File.open(File.join(home, "views", "slide.haml")) do |f|
 			engine = Haml::Engine.new(f.read, :format => :xhtml).render(Object.new, :images => images)
 		end
-		File.write(File.join(home, "uploads", "#{@filename.split('.')[0]}", "#{@filename.split('_')[1]}.html"), engine) if !engine.nil?
+		File.write(targetDirPath, "#{@filename.split('_')[0..-1].join('_')}.html"), engine) if !engine.nil?
 	end
 	
 	def uploadedFilePath
+		targetDirPath << '/' << @filename
+	end
+
+	def uploadedDirPath
 		home = File.join(File.dirname(__FILE__), '..')
 		targetDirPath = File.join(home, "uploads", "#{@filename.split('.')[0]}")
 		Dir.mkdir(targetDirPath) if !Dir.exist?(targetDirPath)
-		targetDirPath << '/' << @filename
+		targetDirPath
 	end
 end
