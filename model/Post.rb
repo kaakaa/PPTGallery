0b0de6517af1da862dfe6e0daf3da77577b1a298
kaakaa <stooner.hoe@gmail.com>
2013-12-25 13:18:10 +0900

    #5 add jp timezone setting

--- model/Post.rb
+++ model/Post.rb
@@ -6,6 +6,6 @@ class Post
 		@link = "http://#{url}"
 		@guid = "http://#{url}#{dirname}/#{dirname.split('/')[-1].split('_')[1..-2].join('_')}.html"
 		@description = "uploaded slide"
-		@pubDate = DateTime.strptime(dirname.split('/')[-1].split('_')[0], "%Y%m%d%H%M%S")
+		@pubDate = DateTime.strptime(dirname.split('/')[-1].split('_')[0], "%Y%m%d%H%M%S").new_offset(Rational(-9,24))
 	end
 end
