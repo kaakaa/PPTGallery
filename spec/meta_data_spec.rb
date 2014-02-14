faa607e1b0bbc9edb14c30002380488221682764
vagrant <stooner.hoe@gmail.com>
2014-02-14 12:29:56 +0000

    delete unnecessary files & rename spec file

--- spec/meta_data_spec.rb
+++ spec/meta_data_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '..', 'model', 'MetaData.rb')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'meta_data')
 
 describe MetaData do
 	it "should get dirname" do
