86011903016df3946b706d58b9b8cf5120df4442
kaakaa <stooner.hoe@gmail.com>
2015-06-05 23:17:56 +0900

    fix for heroku

--- lib/pptgallery/my_logger.rb
+++ lib/pptgallery/my_logger.rb
@@ -3,7 +3,7 @@ require 'logger'
 module MyLogger extend self
   attr_accessor :log
 
-  self.log = Logger.new('log/application.log')
+  self.log = Logger.new('application.log')
   self.log.level = Logger::DEBUG
   self.log.formatter = proc { |severity, datetime, progname, msg|
     "#{severity} : #{datetime.strftime('%Y-%m-%d %H:%M:%S')} :: #{msg}\n"
